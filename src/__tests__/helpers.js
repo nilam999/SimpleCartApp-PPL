63aa120431d99285912eaad407a9d3452ad70528
nilamyuniari <nilamyuniari99@gmail.com>
2020-09-30 21:46:19 +0700

    tugas ppl simple cart

---
+++ src/__tests__/helpers.js
@@ -0,0 +1,18 @@
+/*
+    Gunakan file ini untuk membuat test case dengan Jest
+*/
+const helper = require('../helpers');
+
+test('Jika qty adalah 6, ketika dikurangi 1 jadi = 5', () => {
+    expect(helper.decrementQty(6)).toBe(5);
+});
+
+test('Jika qty adalah 6, ketika ditambah 1 jadi = 7', () => {
+    expect(helper.incrementQty(6)).toBe(7);
+});
+
+test('Jika qtynya adalah 1, decrement button disabled = true', () => {
+    expect(helper.disableDecrement(1)).toBeTruthy();
+});
+
+
